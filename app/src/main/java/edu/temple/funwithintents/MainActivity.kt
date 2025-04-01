96789f74be8f0a295f5cb0768abd7124ae596c53
Annie Gleason <tun23140@temple.edu>
2025-04-01 11:56:31 -0400

    Added Intents for text resources.

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,9 +1,12 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +18,15 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            if(editText.text == null) {
+                Toast.makeText(this, "Empty Text", Toast.LENGTH_LONG).show()
+            } else {
+                val intent = Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, editText.text.toString())
+                }
+                startActivity(Intent.createChooser(intent, "share via"))
+            }
         }
     }
 }
